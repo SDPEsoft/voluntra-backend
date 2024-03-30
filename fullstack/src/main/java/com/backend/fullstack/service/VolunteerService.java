2d4ea69d72d8e00b78a940a774c5adcdadbfe6ff
Sai Charan <sc16101@gmail.com>
2024-03-30 18:47:56 +0530

    Service and Implementation

---
+++ fullstack/src/main/java/com/backend/fullstack/service/VolunteerService.java
@@ -0,0 +1,7 @@
+package com.backend.fullstack.service;
+
+import com.backend.fullstack.dto.VolunteerDto;
+
+public interface VolunteerService {
+    VolunteerDto createVolunteer(VolunteerDto volunteerDto);
+}
---
+++ fullstack/src/main/java/com/backend/fullstack/service/impl/VolunteerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.backend.fullstack.service.impl;
+
+import com.backend.fullstack.dto.VolunteerDto;
+import com.backend.fullstack.entity.Volunteer;
+import com.backend.fullstack.mapper.VolunteerMapper;
+import com.backend.fullstack.repository.VolunteerRepository;
+import com.backend.fullstack.service.VolunteerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VolunteerServiceImpl implements VolunteerService {
+    private VolunteerRepository volunteerRepository;
+    @Override
+    public VolunteerDto createVolunteer(VolunteerDto volunteerDto) {
+        Volunteer volunteer = VolunteerMapper.mapTpVolunteer(volunteerDto);
+        Volunteer savedVolunteer= volunteerRepository.save(volunteer);
+        return VolunteerMapper.mapToVolunteerDto(savedVolunteer);
+
+    }
+}

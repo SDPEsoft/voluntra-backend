9c6231fb98f1c6ed7f47b29b004c1983610daa65
Sai Charan <sc16101@gmail.com>
2024-04-03 12:50:45 +0530

    Automated Email Set

--- fullstack/src/main/java/com/backend/fullstack/FullstackApplication.java
+++ fullstack/src/main/java/com/backend/fullstack/FullstackApplication.java
@@ -1,13 +1,25 @@
 package com.backend.fullstack;
 
+import com.backend.fullstack.service.impl.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FullstackApplication {
+	@Autowired
+	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
+
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendEmail("sai.charan73@yahoo.com","This is subject","This is Body of email");
+
 	}
 
 }

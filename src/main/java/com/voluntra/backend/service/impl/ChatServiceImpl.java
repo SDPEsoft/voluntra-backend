d55d786e4c01dffb6350732faca5ed0e6c8af87a
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-04-17 11:12:04 +0530

    changes added

--- src/main/java/com/voluntra/backend/service/impl/ChatServiceImpl.java
+++ src/main/java/com/voluntra/backend/service/impl/ChatServiceImpl.java
@@ -3,8 +3,13 @@ package com.voluntra.backend.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.voluntra.backend.dto.ChatDto;
 import com.voluntra.backend.entity.ChatEntity;
+import com.voluntra.backend.entity.OrganizationEntity;
+import com.voluntra.backend.entity.UserEntity;
 import com.voluntra.backend.repository.ChatRepository;
+import com.voluntra.backend.repository.OrganizationRepository;
+import com.voluntra.backend.repository.UserRepository;
 import com.voluntra.backend.service.ChatService;
 
 @Service
@@ -13,9 +18,25 @@ public class ChatServiceImpl implements ChatService{
     @Autowired
     private ChatRepository chatRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
     @Override
-    public ChatEntity createChat(ChatEntity chatEntity) {
-        return chatRepository.save(chatEntity);
+    public ChatEntity createChat(ChatDto chatDto) {
+        OrganizationEntity organizationEntity = organizationRepository.findById(chatDto.getOrganizationId()).orElse(null);
+        UserEntity userEntity = userRepository.findById(chatDto.getVolunteerId()).orElse(null);
+        if (organizationEntity!=null && userEntity!=null) {
+            ChatEntity chatEntity = new ChatEntity();
+            chatEntity.setMessage(chatDto.getMessage());
+            chatEntity.setOrganizationEntity(organizationEntity);
+            chatEntity.setVolunteerEntity(userEntity);
+            return chatRepository.save(chatEntity);
+        } else {
+            return null;
+        }
     }
 
     @Override
